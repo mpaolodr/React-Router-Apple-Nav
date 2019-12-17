a85478a094f8bf95ba22666686f7bcc402a263a3
mpaolodr <mpaolodr_1504@yahoo.com>
2019-12-17 17:43:40 -0500

    conflicts might emerge. refactor branch changes are to be prioritized

--- apple-nav/src/components/SubLink.js
+++ apple-nav/src/components/SubLink.js
@@ -1,46 +1,18 @@
 import React from "react";
-import { Link, Switch, Route } from "react-router-dom";
+import { Link, Switch, Route, useParams } from "react-router-dom";
+
+import LaptopIcon from "@material-ui/icons/Laptop";
 
 const SubLink = ({ main }) => {
-  const [Mac, iPad, iPhone, Watch, TV, Music, Support] = main;
+  const { id } = useParams();
+
+  const appleItem = main.find(item => item.name === `${id}`);
 
   return (
     <div className="subLink">
-      <Switch>
-        <Route path={`/${Mac.name}/`}>
-          {Mac.value.map(link => {
-            return <Link>{link}</Link>;
-          })}
-        </Route>
-        <Route path={`/${iPad.name}/`}>
-          {iPad.value.map(link => {
-            return <Link>{link}</Link>;
-          })}
-        </Route>
-        <Route path={`/${iPhone.name}/`}>
-          {iPhone.value.map(link => {
-            return <Link>{link}</Link>;
-          })}
-        </Route>
-        <Route path={`/${Watch.name}/`}>
-          {Watch.value.map(link => {
-            return <Link>{link}</Link>;
-          })}
-        </Route>
-        <Route path={`/${TV.name}/`}>
-          {TV.value.map(link => {
-            return <Link>{link}</Link>;
-          })}
-        </Route>
-        <Route path={`/${Music.name}/`}>
-          {Music.value.map(link => {
-            return <Link>{link}</Link>;
-          })}
-        </Route>
-        <Route path={`/${Support.name}/`}>
-          <Link>Maybe Some form</Link>
-        </Route>
-      </Switch>
+      {appleItem.value.map(link => {
+        return <Link>{link}</Link>;
+      })}
     </div>
   );
 };
